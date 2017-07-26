fbd19d3656001f93e9f601d8fc85db1775472e23
Brandon Stewart <boveus@gmail.com>
2017-07-26 09:39:43 -0600

    Add working total cost for reunion.

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -9,5 +9,12 @@ class Reunion
 
   def add_activity(activity)
     @activities << activity
-  end 
+  end
+
+  def total_cost(total_cost = 0)
+    @activities.each do |activity|
+      total_cost += activity.total_cost
+    end
+    total_cost
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -29,4 +29,38 @@ class ReunionTest < Minitest::Test
 
     assert_instance_of Activity, @reunion.activities[0]
   end
+
+  def test_reunion_total_cost
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Jim", 20)
+    activity_1.add_participant("Joe", 40)
+    @reunion.add_activity(activity_1)
+
+    assert_equal 60, @reunion.total_cost
+  end
 end
+#
+# > reunion = Reunion.new("Denver")
+# # => #<Reunion:0x007fe4ca1defc8 ...>
+#
+# > activity_1 = Activity.new("Brunch")
+# > activity_1.add_participant("Jim", 20)
+# > activity_1.add_participant("Joe", 40)
+# > reunion.add_activity(activity_1)
+# > reunion.total_cost
+# # => 60
+#
+# > activity_2 = Activity.new("Drinks")
+# > activity_2.add_participant("Jim", 60)
+# > activity_2.add_participant("John", 80)
+# > reunion.add_activity(activity_2)
+# > reunion.total_cost
+# # => 200
+#
+# > reunion.breakout
+# # => {"Jim"=> 20, "Joe"=> -10, "John"=> -10}
+#
+# > reunion.print_summary
+# Jim: 20
+# Joe: -10
+# John: -10
