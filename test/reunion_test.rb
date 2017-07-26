9097f6448216cf7c3ac41866b4a10cdff41a2ff5
Brandon Stewart <boveus@gmail.com>
2017-07-26 09:36:43 -0600

    Add working iteration 3 with tests.

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
   def setup
@@ -19,18 +20,13 @@ class ReunionTest < Minitest::Test
   def test_reunion_activities_start_empty
     assert_equal 0, @reunion.activities.count
   end
+
+  def test_add_activity
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Jim", 20)
+    activity_1.add_participant("Joe", 40)
+    @reunion.add_activity(activity_1)
+
+    assert_instance_of Activity, @reunion.activities[0]
+  end
 end
-
-
-
-# > reunion.activities
-# # => []
-#
-# > activity_1 = Activity.new("Brunch")
-# # => #<Activity:0x007fe4ca1d9438 ...>
-#
-# > activity_1.add_participant("Jim", 20)
-# > activity_1.add_participant("Joe", 40)
-# > reunion.add_activity(activity_1)
-# > reunion.activities
-# # => [#<Activity:0x007fe4ca1d9438 ...>]
