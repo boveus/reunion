d3913ff6963f54801f140db8ff10be4cbde3eaa7
Brandon Stewart <boveus@gmail.com>
2017-07-26 09:04:53 -0600

    Add first two passing tests for Activity.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,41 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @activity = Activity.new("Brunch")
+  end
+
+  def test_instance_of_activity
+    assert_instance_of Activity, @activity
+  end
+
+  def test_retrieve_name_of_activity
+    assert_equal "Brunch", @activity.name
+  end
 end
+
+
+
+#
+# > require './lib/activity'
+# # => true
+#
+# > activity = Activity.new("Brunch")
+# # => #<Activity:0x007fe4ca1df568 ...>
+#
+# > activity.name
+# # => "Brunch"
+#
+# > activity.participants
+# # => {}
+#
+# > activity.add_participant("Jim", 20)
+# > activity.participants
+# # => {"Jim" => 20}
+#
+# > activity.total_cost
+# # => 20
+#
+# > activity.add_participant("Joe", 40)
+# > activity.participants
+# # => {"Jim" => 20, "Joe" => 40}
