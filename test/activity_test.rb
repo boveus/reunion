4bf59649251a43b6e09a60cd39eb8cf4f16e4857
Brandon Stewart <boveus@gmail.com>
2017-07-26 09:12:04 -0600

    Remove comments from test.

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -37,29 +37,3 @@ class ActivityTest < Minitest::Test
     assert_equal activity_new.total_cost, 60
   end
 end
-
-
-
-#
-# > require './lib/activity'
-# # => true
-#
-# > activity = Activity.new("Brunch")
-# # => #<Activity:0x007fe4ca1df568 ...>
-#
-# > activity.name
-# # => "Brunch"
-#
-# > activity.participants
-# # => {}
-#
-# > activity.add_participant("Jim", 20)
-# > activity.participants
-# # => {"Jim" => 20}
-#
-# > activity.total_cost
-# # => 20
-#
-# > activity.add_participant("Joe", 40)
-# > activity.participants
-# # => {"Jim" => 20, "Joe" => 40}
